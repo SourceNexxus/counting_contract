1f9720a8ab4fc80fea6e374489aae0c2b1e61719
SourceNexxus <cory@sourceprotocol.io>
2022-12-29 15:41:32 -0700

    3

---
+++ counting-contract-0.3/src/bin/schema.rs
@@ -0,0 +1,9 @@
+use cosmwasm_schema::write_api;
+use counting_contract::msg::{ExecMsg, InstantiateMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecMsg,
+    }
+}
